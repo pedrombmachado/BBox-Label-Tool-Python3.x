0047fe5c0e5da2f7004541822347d2c45461771c
Pedro Machado <pedro.baptistamachado@ntu.ac.uk>
2019-02-22 17:38:22 +0000

    Update main.py
    Updated the jpg issue

--- main.py
+++ main.py
@@ -133,7 +133,6 @@ class LabelTool():
 ##            return
         # get image list
         self.imageDir = os.path.join(r'./Images', '%03d' %(self.category))
-        self.imageDir = os.path.join(r'./Images', '%03d' %(self.category))
         self.imageList = glob.glob(os.path.join(self.imageDir, '*.jpg')) + glob.glob(os.path.join(self.imageDir, '*.jpeg')) \
                 + glob.glob(os.path.join(self.imageDir, '*.JPG')) + glob.glob(os.path.join(self.imageDir, '*.JPEG'))
         self.imageList.sort()  # By Tomonori12
